a7550d3bafa13e22278c7efac6781d40e01cc9b1
Alex Vasilev <bestspajic@gmail.com>
2013-08-08 09:18:42 -0700

    Background: Given movies in DB

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    title, rating, release_date = movie
+    Movie.find_or_create_by_title_and_rating_and_release_date(title, rating, release_date)
   end
-  flunk "Unimplemented"
+  # flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
